479424560029dfa5fe525bd5ee63717468beb1a3
heoinseo <112676998+heoinseo@users.noreply.github.com>
2022-10-07 23:41:13 +0900

    Add files via upload

---
+++ 22_05_1st_Report.py
@@ -0,0 +1,27 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("환영합니다.")
+환영합니다.
+print("파이썬의 세계에 오신 것을 환영합니다.")
+파이썬의 세계에 오신 것을 환영합니다.
+print("파이썬은 강력합니다.")
+파이썬은 강력합니다.
+
+반갑습니다. 파이썬!
+SyntaxError: invalid syntax
+0.6
+0.6
+Hello World!!!
+SyntaxError: invalid syntax
+print("한 주는",7*24","시간이다.")
+      
+SyntaxError: unterminated string literal (detected at line 1)
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+
+t.fd(100);t.lt(90);t.fd(100);t.rt(90);t.fd(100);
+t.rt(90);t.fd(100);t.lt(90);t.fd(100)
+
+turtle.mainloop()
+turtle.bye()
